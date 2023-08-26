abde0b986c8d49c567cbf210dc03b5e407920c7b
ansufei <annesophie.meyer@gmail.com>
2023-08-26 16:22:30 +0200

    folder for adventure games (where you are the hero)

---
+++ adventure/birthday_party.py
@@ -0,0 +1,32 @@
+	
+#!/usr/bin/env python 
+
+import random
+import time
+def display_intro():
+    print("What's that??!")
+    time.sleep(2)
+    print("James' shout was half lost in the noise of the party, in full-swing now as the heavy drinking had started 2 hours ago")
+    time.sleep(3)
+    print("I made an effort to look interested. James was showing me a picture on his phone.")
+    time.sleep(2)
+    print("It was apparently sent to the messaging group organized for his birtday party")
+    time.sleep(3)
+    cake=open('cake.txt')
+    print(cake.read())
+    time.sleep(2)
+    print("\"Who did that to my cake?\", James wrote in the group chat")
+    time.sleep(2)
+    print("Uh oh. The thing was a mess of cream and crust spread all over the floor.")
+    time.sleep(2)
+    print("Clearly, the serving plate had been toppled.")
+    time.sleep(2)
+    print("But who touched the cake? The candle blowing and cake eating ceremony wasn't due until 9pm, 1 hour from now")
+    time.sleep(2)
+    print('''This was serious matter. Quickly we organized a search and finger-pointing mission. The efficient staff had used video recording to 
+    identify who had entered and left the garage, where the fridge was that contained the cake. The last time someone had reliably checked on the cake was at 7:30pm so the 
+    suspects were all the people seen entering or exiting the garage through one of its two doors after that time: 7 suspects in total. The party-goers were grouped 
+    into teams interviewing the suspects and collecting cues.''')
+    time.sleep(10)
+    print('You were assigned into team 1. Good luck in finding the culprit! I\'ve been told that the winning team will get a piece from the backup cake...')
+
